1425db927a30bc8cb6476555b169b0f553920439
Ebuka <ebuka2264@yahoo.com>
2024-01-29 10:11:04 +0100

    feat: include stellar blockchain

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const bip39 = require("bip39");
 const HDKey = require("hdkey");
 const CoinKey = require("coinkey");
 const ethers = require("ethers");
-const eu = require('ethereumjs-util')
+const eu = require('ethereumjs-util');
+//const StellarSdk = require("js-stellar-sdk");
+var StellarSdk = require('@stellar/stellar-sdk');
+
 
 
 
@@ -15,7 +18,6 @@ const seed_phrase = "monster biology normal element armor subject misery coyote 
 
 const TESTNET = bitcoin.networks.testnet;
 const ECPair = ECPairFactory(ecc);
-
 
 async function btc_addr(mnemonic){    
     const seed = bip39.mnemonicToSeedSync(mnemonic);
@@ -28,7 +30,13 @@ async function btc_addr(mnemonic){
         path,                                    // BIP44 path
         privateKey: coinKey.privateKey.toString("hex"), // Private key in hexadecimal
         WIF: coinKey.privateWif,                 // Wallet Import Format (WIF) private key
-      };    
+      }; 
+
+    // Generate a testnet address  
+    //const { address } = bitcoin.payments.p2pkh({ pubkey: child.publicKey, network: TESTNET, });
+    
+    // generate a segwit Address
+    //const { address } = bitcoin.payments.p2wpkh({ pubkey: child.publicKey });  
     console.log(info)
     return info
 }
@@ -48,17 +56,41 @@ async function eth_addr(mnemonic){
     console.log(info)
     return info
 
+}
+
+async function stellar(mnemonic){
+    const seed = bip39.mnemonicToSeedSync(mnemonic);
+    const hdKey = HDKey.fromMasterSeed(Buffer.from(seed, "hex"));
+    const path = "m/44'/148'/0'/0/0"
+    const child = hdKey.derive(path);
+    const KeyPair = StellarSdk.Keypair.fromRawEd25519Seed(child.privateKey)
+    const secret = KeyPair.secret()   
+    const info = {
+        address: KeyPair.publicKey() ,
+        path,                                   
+        secret,
+      }; 
+
+    console.log(info)
+    return(info)
 
 }
 
 
-
-
-
-
 async function main(mnemonic){
-    //await btc_addr(mnemonic)
+    console.log("seed: ", mnemonic)
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    console.log("++++++++++++++++++++++++++++++BITCOIN IMPLEMENTATION+++++++++++++++++++++++++++++++++++++++++++++++")
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    await btc_addr(mnemonic)
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    console.log("++++++++++++++++++++++++++++++ETHEREUM IMPLEMENTATION+++++++++++++++++++++++++++++++++++++++++++++++")
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     let buffer = await eth_addr(mnemonic);
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    console.log("++++++++++++++++++++++++++++++STELLAR IMPLEMENTATION+++++++++++++++++++++++++++++++++++++++++++++++")
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    await stellar(mnemonic)
 
 }
 

9153e8a91aa225d2da597d7534ef0627d3b7659d
BukiOffor <ebuka2264@yahoo.com>
2024-03-06 21:16:39 +0100

    fix: change networks

--- scripts.js
+++ scripts.js
@@ -108,15 +108,24 @@ async function bitcoin_wallet(mnemonic){
             const path = "m/44'/0'/0'/0/0";
             const child = hdKey.derive(path);
             const coinKey = new CoinKey(child.privateKey, bitcoin.networks.bitcoin);
-            const info = {
-            address: coinKey.publicAddress,          // Bitcoin public address
-            path,                                    // BIP44 path
-            privateKey: coinKey.privateKey.toString("hex"), // Private key in hexadecimal
-            WIF: coinKey.privateWif,                 // Wallet Import Format (WIF) private key
-            }; 
+            
+            // MainNet Account
+            
+            // const info = {
+            // address: coinKey.publicAddress,          // Bitcoin public address
+            // path,                                    // BIP44 path
+            // privateKey: coinKey.privateKey.toString("hex"), // Private key in hexadecimal
+            // WIF: coinKey.privateWif,                 // Wallet Import Format (WIF) private key
+            // }; 
 
             // Generate a testnet address  
-            //const { address } = bitcoin.payments.p2pkh({ pubkey: child.publicKey, network: TESTNET, });
+            const { address } = bitcoin.payments.p2pkh({ pubkey: child.publicKey, network: TESTNET, });
+            const info = {
+                address: address,          // Bitcoin public address
+                path,                                    // BIP44 path
+                privateKey: coinKey.privateKey.toString("hex"), // Private key in hexadecimal
+                WIF: coinKey.privateWif,                 // Wallet Import Format (WIF) private key
+                }; 
     
             // generate a segwit Address
             //const { address } = bitcoin.payments.p2wpkh({ pubkey: child.publicKey });  
@@ -334,7 +343,64 @@ async function sendErcTokens(privateKey, contractTokenAddress, amount, recipient
     }
   };
     
+async function sendStellarXlm(privateKey, destinationId){
+    
+    var server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
+    var sourceKeys = StellarSdk.Keypair.fromSecret(privateKey);
+    // Transaction will hold a built transaction we can resubmit if the result is unknown.
+    var transaction;
 
+    // First, check to make sure that the destination account exists.
+    // You could skip this, but if the account does not exist, you will be charged
+    // the transaction fee when the transaction fails.
+    server
+    .loadAccount(destinationId)
+    // If the account is not found, surface a nicer error message for logging.
+    .catch(function (error) {
+        if (error instanceof StellarSdk.NotFoundError) {
+        throw new Error("The destination account does not exist!");
+        } else return error;
+    })
+    // If there was no error, load up-to-date information on your account.
+    .then(function () {
+        return server.loadAccount(sourceKeys.publicKey());
+    })
+    .then(function (sourceAccount) {
+        // Start building the transaction.
+        transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
+        fee: StellarSdk.BASE_FEE,
+        networkPassphrase: StellarSdk.Networks.TESTNET,
+        })
+        .addOperation(
+            StellarSdk.Operation.payment({
+            destination: destinationId,
+            // Because Stellar allows transaction in many currencies, you must
+            // specify the asset type. The special "native" asset represents Lumens.
+            asset: StellarSdk.Asset.native(),
+            amount: "10",
+            }),
+        )
+        // A memo allows you to add your own metadata to a transaction. It's
+        // optional and does not affect how Stellar treats the transaction.
+        .addMemo(StellarSdk.Memo.text("Test Transaction"))
+        // Wait a maximum of three minutes for the transaction
+        .setTimeout(180)
+        .build();
+        // Sign the transaction to prove you are actually the person sending it.
+        transaction.sign(sourceKeys);
+        // And finally, send it off to Stellar!
+        return server.submitTransaction(transaction);
+    })
+    .then(function (result) {
+        console.log("Success! Results:", result);
+    })
+    .catch(function (error) {
+        console.error("Something went wrong!", error);
+        // If the result is unknown (no response body, timeout etc.) we simply resubmit
+        // already built transaction:
+        // server.submitTransaction(transaction);
+    });
+}
 
 
 
@@ -346,7 +412,8 @@ module.exports = {
     sendTransactionEth,
     sendBitcoin,
     sendErcTokens,
-    getErcTokensBalance
+    getErcTokensBalance,
+    sendStellarXlm
 }
 
 
